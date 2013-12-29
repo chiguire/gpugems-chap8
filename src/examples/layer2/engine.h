dff6ee0782d5f35cb51f87e1308f6e0332ec8747
Ciro Duran <ciro.duran@gmail.com>
2013-12-29 18:14:58 +0000

    Completed reflection with a cube map. Had to swap posy and negy because of shader. Will fix later because there are still some jaggies around the cube.

--- src/examples/layer2/engine.h
+++ src/examples/layer2/engine.h
@@ -13,27 +13,38 @@ namespace octet {
     typedef mat4t mat4t;
     typedef vec4 vec4;
 
+    vec3 camera_position;
+    vec3 camera_rotation;
+
     float rotateAngle;
     mat4t cameraToWorld;
     mat4t modelToWorld;
 
     // helper to rotate camera about scene
-    mouse_ball ball;
+    //mouse_ball ball;
 
     GLuint cubeMapTex;
+    GLuint leMapTex;
 
-    cubemap_shader cubeMapShader;
+    cubemap_reflection_shader cubeMapReflectionShader;
+    cubemap_sky_shader cubeMapSkyShader;
 
   public:
     // this is called when we construct the class
-    engine(int argc, char **argv) : app(argc, argv), ball() {
+    engine(int argc, char **argv)
+    : app(argc, argv)
+    , camera_position(0.0f, 0.0f, 3.0f)
+    , camera_rotation(0.0f, 0.0f, 0.0f)
+    //, ball()
+    {
 
     }
 
     // this is called once OpenGL is initialized
     void app_init() {
       // set up the shaders
-      cubeMapShader.init();
+      cubeMapReflectionShader.init();
+      cubeMapSkyShader.init();
 
       cameraToWorld.loadIdentity();
       modelToWorld.loadIdentity();
@@ -41,8 +52,10 @@ namespace octet {
       rotateAngle = 0;
 
       cubeMapTex = resources::get_cubemap_texture_handle(GL_RGBA, "Tenerife4",
-        "assets/cubemaps/Tenerife4/posx.jpg", "assets/cubemaps/Tenerife4/posy.jpg", "assets/cubemaps/Tenerife4/posz.jpg",
-        "assets/cubemaps/Tenerife4/negx.jpg", "assets/cubemaps/Tenerife4/negy.jpg", "assets/cubemaps/Tenerife4/negz.jpg");
+        "assets/cubemaps/Tenerife4/posx.jpg", "assets/cubemaps/Tenerife4/negy.jpg", "assets/cubemaps/Tenerife4/posz.jpg",
+        "assets/cubemaps/Tenerife4/negx.jpg", "assets/cubemaps/Tenerife4/posy.jpg", "assets/cubemaps/Tenerife4/negz.jpg");
+
+      printf("Cube map tex: %d\n", cubeMapTex);
     }
 
     // this is called to draw the world
@@ -53,38 +66,185 @@ namespace octet {
       glViewport(0, 0, vx, vy);
 
       // clear the background to black
-      glClearColor(0.5f, 0.5f, 0.5f, 1);
+      glClearColor(0.5f, 0.2f, 0.2f, 1);
       glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-      // allow Z buffer depth testing (closer objects are always drawn in front of far ones)
+      renderSky();
+
+      renderCD();
+      
+      rotateAngle += 1.0f;
+
+      if (is_key_down('W')) {
+        camera_position[1] += 0.25f * (camera_position[2]/5.0f);
+      } else if (is_key_down('S')) {
+        camera_position[1] -= 0.25f * (camera_position[2]/5.0f);
+      }
+
+      if (is_key_down('A')) {
+        camera_position[0] -= 0.25f * (camera_position[2]/5.0f);
+      } else if (is_key_down('D')) {
+        camera_position[0] += 0.25f * (camera_position[2]/5.0f);
+      }
+
+      if (is_key_down('Q')) {
+        camera_position[2] -= 0.25f;
+        if (camera_position[2] < 2.0f) camera_position[2] = 2.0f;
+      } else if (is_key_down('E')) {
+        camera_position[2] += 0.25f;
+        if (camera_position[2] > 200.0f) camera_position[2] = 200.0f;
+      }
+
+       if (is_key_down('F')) {
+        camera_rotation[1] += 5.0f;
+        if (camera_rotation[1] >= 360.0f) camera_rotation[1] -= 360.0f;
+      } else if (is_key_down('H')) {
+        camera_rotation[1] -= 5.0f;
+        if (camera_rotation[1] < 0.0f) camera_rotation[1] += 360.0f;
+      }
+
+      if (is_key_down('G')) {
+        camera_rotation[0] -= 5.0f;
+        if (camera_rotation[0] < -60.0f) camera_rotation[0] = -60.0f;
+      } else if (is_key_down('T')) {
+        camera_rotation[0] += 5.0f;
+        if (camera_rotation[0] > 60.0f) camera_rotation[0] = 60.0f;
+      }
+
+      /*if (is_key_down('R')) {
+        camera_rotation -= 1.0f;
+      } else if (is_key_down('T')) {
+        camera_rotation += 1.0f;
+      }*/
+    }
+
+    void renderSky() {
+      glDisable(GL_DEPTH_TEST);
+
+      mat4t skyModelToWorld;
+      skyModelToWorld.loadIdentity();
+      mat4t skyCameraToWorld;
+      skyCameraToWorld.loadIdentity();
+      skyCameraToWorld.rotate(camera_rotation[1], 0.0f, 1.0f, 0.0f);
+      skyCameraToWorld.rotate(camera_rotation[0], 1.0f, 0.0f, 0.0f);
+
+      mat4t skyModelToProjection = mat4t::build_projection_matrix(skyModelToWorld, skyCameraToWorld);
+
+      glActiveTexture(GL_TEXTURE0);
+      glEnable(GL_TEXTURE_CUBE_MAP);
+      glBindTexture(GL_TEXTURE_CUBE_MAP, cubeMapTex);
+
+      cubeMapSkyShader.render(&skyModelToProjection, &camera_position, 0);
+
+      float skyCubeVertices[] =
+      {    // x, y, z
+        1.0f, -1.0f, -1.0f,
+        1.0f, -1.0f,  1.0f,
+        1.0f,  1.0f,  1.0f,
+        1.0f,  1.0f, -1.0f,
+        -1.0f, -1.0f,  1.0f,
+        -1.0f, -1.0f, -1.0f,
+        -1.0f,  1.0f, -1.0f,
+        -1.0f,  1.0f,  1.0f,
+        -1.0f,  1.0f, -1.0f,
+        1.0f,  1.0f, -1.0f,
+        1.0f,  1.0f,  1.0f,
+        -1.0f,  1.0f,  1.0f,
+        -1.0f, -1.0f,  1.0f,
+        1.0f, -1.0f,  1.0f,
+        1.0f, -1.0f, -1.0f,
+        -1.0f, -1.0f, -1.0f,
+        1.0f, -1.0f,  1.0f,
+        -1.0f, -1.0f,  1.0f,
+        -1.0f,  1.0f,  1.0f,
+        1.0f,  1.0f,  1.0f,
+        -1.0f, -1.0f, -1.0f,
+        1.0f, -1.0f, -1.0f,
+        1.0f,  1.0f, -1.0f,
+        -1.0f,  1.0f, -1.0f
+      };
+
+      glVertexAttribPointer(attribute_pos, 3, GL_FLOAT, GL_FALSE, 0, skyCubeVertices);
+
+      glEnableVertexAttribArray(attribute_pos);
+
+      glDrawArrays(GL_TRIANGLE_FAN, 0, 4);
+      glDrawArrays(GL_TRIANGLE_FAN, 4, 4);
+      glDrawArrays(GL_TRIANGLE_FAN, 8, 4);
+      glDrawArrays(GL_TRIANGLE_FAN, 12, 4);
+      glDrawArrays(GL_TRIANGLE_FAN, 16, 4);
+      glDrawArrays(GL_TRIANGLE_FAN, 20, 4);
+
+      glDisableVertexAttribArray(attribute_pos);
+    }
+
+    void renderCD() {
+      glEnable(GL_TEXTURE_CUBE_MAP);
       glEnable(GL_DEPTH_TEST);
 
       cameraToWorld.loadIdentity();
-      cameraToWorld.translate(0, 0, 3.0f);
+      cameraToWorld.rotate(camera_rotation[1], 0.0f, 1.0f, 0.0f);
+      cameraToWorld.rotate(camera_rotation[0], 1.0f, 0.0f, 0.0f);
+      cameraToWorld.translate(camera_position.x(), camera_position.y(), camera_position.z());
       
       modelToWorld.loadIdentity();
       modelToWorld.rotate(rotateAngle, 0.0f, 1.0f, 0.0f);
 
       mat4t modelToProjection = mat4t::build_projection_matrix(modelToWorld, cameraToWorld);
 
-      cubeMapShader.render(modelToProjection, cubeMapTex);
+      glActiveTexture(GL_TEXTURE0);
+      glEnable(GL_TEXTURE_CUBE_MAP);
+      glBindTexture(GL_TEXTURE_CUBE_MAP, cubeMapTex);
+      
+      cubeMapReflectionShader.render(&modelToProjection, &modelToWorld, 0);
 
       float vertices[] = {
-        -1.0f,  1.0f, 0.0f, 0.0f, 0.0f, 1.0f,
-        -1.0f, -1.0f, 0.0f, 0.0f, 0.0f, 1.0f,
-         1.0f, -1.0f, 0.0f, 0.0f, 0.0f, 1.0f,
-         1.0f,  1.0f, 0.0f, 0.0f, 0.0f, 1.0f
+        1.0f,  1.0f, -1.0f, -1.0f, -1.0f, 1.0f,
+        1.0f,  1.0f,  1.0f, -1.0f, -1.0f, -1.0f,
+        1.0f, -1.0f,  1.0f, -1.0f, 1.0f, -1.0f,
+        1.0f, -1.0f, -1.0f, -1.0f, 1.0f, 1.0f,
+
+        -1.0f,  1.0f,  1.0f, 1.0f, -1.0f, -1.0f,
+        -1.0f,  1.0f, -1.0f, 1.0f, -1.0f, 1.0f, 
+        -1.0f, -1.0f, -1.0f, 1.0f, 1.0f, 1.0f,
+        -1.0f, -1.0f,  1.0f, 1.0f, 1.0f, -1.0f,
+
+        -1.0f,  1.0f, -1.0f, 1.0f, -1.0f, 1.0f,
+        1.0f,  1.0f, -1.0f, -1.0f, -1.0f, 1.0f,
+        1.0f,  1.0f,  1.0f, -1.0f, -1.0f, -1.0f,
+        -1.0f,  1.0f,  1.0f, 1.0f, -1.0f, -1.0f,
+
+        -1.0f, -1.0f, -1.0f, 1.0f, 1.0f, 1.0f,
+        1.0f, -1.0f, -1.0f, -1.0f, 1.0f, 1.0f,
+        1.0f, -1.0f,  1.0f, -1.0f, 1.0f, -1.0f,
+        -1.0f, -1.0f,  1.0f, 1.0f, 1.0f, -1.0f,
+        
+        1.0f,  1.0f,  1.0f, -1.0f, -1.0f, -1.0f,
+        -1.0f,  1.0f,  1.0f, 1.0f, -1.0f, -1.0f,
+        -1.0f, -1.0f,  1.0f, 1.0f, 1.0f, -1.0f,
+        1.0f, -1.0f,  1.0f, -1.0f, 1.0f, -1.0f,
+        
+        -1.0f,  1.0f, -1.0f, 1.0f, -1.0f, 1.0f,
+        1.0f,  1.0f, -1.0f, -1.0f, -1.0f, 1.0f,
+        1.0f, -1.0f, -1.0f, -1.0f, 1.0f, 1.0f,
+        -1.0f, -1.0f, -1.0f, 1.0f, 1.0f, 1.0f
       };
 
       glVertexAttribPointer(attribute_pos, 3, GL_FLOAT, GL_FALSE, 6*sizeof(float), vertices);
-      glVertexAttribPointer(attribute_normal, 3, GL_FLOAT, GL_TRUE, 6*sizeof(float), vertices+3);
+      glVertexAttribPointer(attribute_normal, 3, GL_FLOAT, GL_FALSE, 6*sizeof(float), vertices+3);
 
       glEnableVertexAttribArray(attribute_pos);
       glEnableVertexAttribArray(attribute_normal);
-      
-      glDrawArrays(GL_TRIANGLE_FAN, 0, 4);
 
-      rotateAngle += 1.0f;
+      glDrawArrays(GL_TRIANGLE_FAN, 0, 4);
+      glDrawArrays(GL_TRIANGLE_FAN, 4, 4);
+      glDrawArrays(GL_TRIANGLE_FAN, 8, 4);
+      glDrawArrays(GL_TRIANGLE_FAN, 12, 4);
+      glDrawArrays(GL_TRIANGLE_FAN, 16, 4);
+      glDrawArrays(GL_TRIANGLE_FAN, 20, 4);
+
+      glDisableVertexAttribArray(attribute_pos);
+      glDisableVertexAttribArray(attribute_normal);
     }
   };
 }
--- src/shaders/cubemap_shader.h
+++ src/shaders/cubemap_shader.h
@@ -7,11 +7,11 @@
 // Single texture shader with no lighting
 
 namespace octet {
-  class cubemap_shader : public shader {
-    // indices to use with glUniform*()
+  class cubemap_reflection_shader : public shader {
 
     // index for model space to projection space matrix
     GLuint modelToProjectionIndex_;
+    GLuint modelToWorldIndex_;
 
     // index for texture sampler
     GLuint samplerIndex_;
@@ -22,13 +22,19 @@ namespace octet {
       // it inputs pos and uv from each corner
       // it outputs gl_Position and uv_ to the rasterizer
       const char vertex_shader[] = SHADER_STR(
-        attribute vec4 a_pos;
-        attribute vec3 a_normal;
-        varying vec3 v_normal;
+        varying vec3 normal_;
 
+        attribute vec4 pos;
+        attribute vec3 normal;
+       
+        uniform vec3 cameraPosition;
         uniform mat4 modelToProjection;
+        uniform mat4 modelToWorld;
 
-        void main() { gl_Position = modelToProjection * a_pos; v_normal = a_normal; }
+        void main() {
+          normal_ = (modelToWorld * vec4(normal, 1)).xyz;
+          gl_Position = modelToProjection * pos; 
+        }
       );
 
       // this is the fragment shader
@@ -36,10 +42,13 @@ namespace octet {
       // this is called for every fragment
       // it outputs gl_FragColor, the color of the pixel and inputs uv_
       const char fragment_shader[] = SHADER_STR(
-        /*precision mediump float;*/
-        varying vec3 v_normal;
+        varying vec3 normal_;
+
         uniform samplerCube sampler;
-        void main() { gl_FragColor = textureCube(sampler, v_normal); }
+
+        void main() {
+          gl_FragColor = textureCube(sampler, normal_);
+        }
       );
     
       // use the common shader code to compile and link the shaders
@@ -48,16 +57,70 @@ namespace octet {
 
       // extract the indices of the uniforms to use later
       modelToProjectionIndex_ = glGetUniformLocation(program(), "modelToProjection");
+      modelToWorldIndex_ = glGetUniformLocation(program(), "modelToWorld");
       samplerIndex_ = glGetUniformLocation(program(), "sampler");
     }
 
-    void render(const mat4t &modelToProjection, int sampler) {
+    void render(const mat4t *modelToProjection, const mat4t *modelToWorld, int sampler) {
       // tell openGL to use the program
       shader::render();
 
       // customize the program with uniforms
       glUniform1i(samplerIndex_, sampler);
-      glUniformMatrix4fv(modelToProjectionIndex_, 1, GL_FALSE, modelToProjection.get());
+      glUniformMatrix4fv(modelToProjectionIndex_, 1, GL_FALSE, modelToProjection->get());
+      glUniformMatrix4fv(modelToWorldIndex_, 1, GL_FALSE, modelToWorld->get());
+    }
+  };
+
+  class cubemap_sky_shader : public shader {
+    GLuint modelToProjectionIndex_;
+    GLuint cameraPositionIndex_;
+    GLuint samplerIndex_;
+
+  public:
+    void init() {
+      const char vertex_shader[] = SHADER_STR(
+        attribute vec4 pos;
+
+        uniform vec3 cameraPosition;
+        uniform mat4 modelToProjection;
+
+        varying vec3 uv_;
+
+        void main() {
+          uv_ = vec3(pos.x, -pos.yz);
+          gl_Position = modelToProjection * vec4(pos.xyz + cameraPosition, 1.0); 
+        }
+      );
+
+      const char fragment_shader[] = SHADER_STR(
+        uniform samplerCube sampler;
+
+        varying vec3 uv_;
+
+        void main() {
+          gl_FragColor = textureCube(sampler, uv_);
+        }
+      );
+    
+      // use the common shader code to compile and link the shaders
+      // the result is a shader program
+      shader::init(vertex_shader, fragment_shader);
+
+      // extract the indices of the uniforms to use later
+      cameraPositionIndex_ = glGetUniformLocation(program(), "cameraPosition");
+      samplerIndex_ = glGetUniformLocation(program(), "sampler");
+      modelToProjectionIndex_ = glGetUniformLocation(program(), "modelToProjection");
+    }
+
+    void render(const mat4t *modelToProjection, const vec3 *cameraPosition, int sampler) {
+      // tell openGL to use the program
+      shader::render();
+
+      // customize the program with uniforms
+      glUniform1i(samplerIndex_, sampler);
+      glUniform3fv(cameraPositionIndex_, 3, cameraPosition->get());
+      glUniformMatrix4fv(modelToProjectionIndex_, 1, GL_FALSE, modelToProjection->get());
     }
   };
 }
